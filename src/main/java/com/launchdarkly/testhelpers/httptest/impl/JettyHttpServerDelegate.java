b6d4f8068f539dfa8bbb848ecad512a1d12821b6
Eli Bishop <eli@launchdarkly.com>
2021-06-22 16:45:47 -0700

    more broadly applicable way of binding

--- src/main/java/com/launchdarkly/testhelpers/httptest/impl/JettyHttpServerDelegate.java
+++ src/main/java/com/launchdarkly/testhelpers/httptest/impl/JettyHttpServerDelegate.java
@@ -100,7 +100,7 @@ public final class JettyHttpServerDelegate implements HttpServer.Delegate {
     sslContextFactory.setValidateCerts(false);
     
     ServerConnector connector = new ServerConnector(server, sslContextFactory);
-    connector.setHost("localhost");
+    connector.setHost("0.0.0.0");
     connector.setPort(port);
     server.setConnectors(new Connector[] { connector });
     
