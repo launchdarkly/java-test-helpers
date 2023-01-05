039a6f90b16faf163ac8da1adc6c370e7f9ae618
Eli Bishop <eli@launchdarkly.com>
2023-01-04 19:27:50 -0800

    once more try to make it like java-logging

--- src/androidTest/java/com/launchdarkly/testhelpers/BaseTest.java
+++ src/androidTest/java/com/launchdarkly/testhelpers/BaseTest.java
@@ -1,6 +1,6 @@
 package com.launchdarkly.testhelpers;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnit4;
 import org.junit.runner.RunWith;
 
 /**
